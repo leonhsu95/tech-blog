3d6ed0edf45c073972e5365822f24f9cf8748872
Leon Hsu <leonhsu95@gmail.com>
2021-06-06 20:56:45 +1000

    Testing Routes and creating index.html for future handlebar template

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,12 +6,6 @@ router.get('/', (req, res) => {
     console.log(req.session);
     
     Post.findAll({
-      attributes: [
-        'id',
-        'title',
-        'created_at',
-        'post_content'
-      ],
       include: [
         {
           model: Comment,
